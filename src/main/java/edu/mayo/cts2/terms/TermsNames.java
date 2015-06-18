0fa953fc0c8d9a929e6f2139d4b13bb01034e81c
sotty <dsotty@gmail.com>
2015-06-18 10:37:08 -0500

    Change namespace

--- src/main/java/edu/mayo/cts2/terms/TermsNames.java
+++ src/main/java/edu/mayo/cts2/terms/TermsNames.java
@@ -5,6 +5,13 @@ public class TermsNames {
 
     public static final String TERMS_METAMODEL_PACKAGE = TermsNames.class.getPackage().getName();
 
-    public static final String EXPRESSES = "http://www.mayo.edu/terms-metamodel/terms#expresses";
+    public static final String EXPRESSES        = "http://edu.mayo.cts2/terms-metamodel/terms#expresses";
+    public static final String IN_SCHEME        = "http://www.w3.org/2004/02/skos/core#inScheme";
+    public static final String NOTATION         = "http://www.w3.org/2004/02/skos/core#notation";
+
+    public static final String CONCEPT_SCHEME   = "http://www.w3.org/2004/02/skos/core#ConceptScheme";
+    public static final String CONCEPT          = "http://www.w3.org/2004/02/skos/core#Concept";
+    public static final String LABEL            = "http://www.w3.org/2004/02/skos/core#prefLabel";
+    public static final String OID              = "https://www.hl7.org/oid";
 
 }
